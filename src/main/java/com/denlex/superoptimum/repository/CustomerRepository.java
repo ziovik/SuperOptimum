8443d5faf373790d106bbf7390b2ea044684e1b4
daniel <mondaydaniel2202@yahoo.com>
2018-08-06 23:02:58 +0300

    Next commit

--- src/main/java/com/denlex/superoptimum/repository/CustomerRepository.java
+++ src/main/java/com/denlex/superoptimum/repository/CustomerRepository.java
@@ -2,9 +2,11 @@ package com.denlex.superoptimum.repository;
 
 import com.denlex.superoptimum.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by Shishkov A.V. on 06.08.18.
  */
+@Repository
 public interface CustomerRepository extends JpaRepository<Long, Customer> {
 }
